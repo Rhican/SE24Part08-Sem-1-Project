f530ca9febebb04c8013fe210936db58f0c2ce60
Rong <Rong@RongAsusA43S>
2016-03-30 02:35:18 +0800

    Initial Gui Commit Demo, home page and product page

---
+++ src/edu/nus/iss/SE24PT8/universityStore/gui/framework/PubSub.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SE24PT8
+ */
+package edu.nus.iss.SE24PT8.universityStore.gui.framework;
+
+public class PubSub {
+    
+}
