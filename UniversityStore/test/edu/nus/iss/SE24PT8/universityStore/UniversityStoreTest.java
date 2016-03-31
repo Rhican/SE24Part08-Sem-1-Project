49c904c27eb5011dbc6a827cd2476949f114b462
Rong Zehua <Rhican.R@gmail.com>
2016-03-31 18:00:52 +0800

    test Commit

---
+++ UniversityStore/test/edu/nus/iss/SE24PT8/universityStore/UniversityStoreTest.java
@@ -0,0 +1,14 @@
+package edu.nus.iss.SE24PT8.universityStore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UniversityStoreTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
