d3ecbc736c1b00ca6c37be206935a78a62b669a1
Rong Zehua <Rhican.R@gmail.com>
2016-04-07 09:11:45 +0800

    order all update

--- UniversityStore/src/edu/nus/iss/SE24PT8/universityStore/gui/components/Checkout/CheckInventoryPanel.java
+++ UniversityStore/src/edu/nus/iss/SE24PT8/universityStore/gui/components/Checkout/CheckInventoryPanel.java
@@ -218,6 +218,7 @@ public class CheckInventoryPanel  extends JPanel implements INotificable{
 					"Success", JOptionPane.INFORMATION_MESSAGE);
 		 }
 		 refersh();
+		 SubjectManager.getInstance().Update("ProductPanel", "Product", "Modify");
 	}
 
 	@Override
